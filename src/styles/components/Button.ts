e0469cd6cfe667de694be9bf9b5b7cbe4d3e4956
Daniel Lucas <daniellucas-pms@hotmail.com>
2021-03-16 13:41:44 -0300

    button transitions

--- src/styles/components/Button.ts
+++ src/styles/components/Button.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Container = styled.button`
   height: 2.5rem;
@@ -14,8 +15,20 @@ export const Container = styled.button`
   font-weight: bold;
   font-family: Roboto;
 
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
   border: none;
   border-radius: 0.5rem;
+
+  transition: background-color 0.2s, box-shadow 0.2s;
+  
+  &:hover {
+    background: ${shade(0.2, '#00A88F')};
+  }
+
+  &:active {
+    box-shadow: none;
+  }
 
   svg {
     margin-right: 1rem;
