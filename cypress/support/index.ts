f0217cb8f9ce3ae56549801bccf0d96854a56a6a
Daniel Lucas <daniellucas-pms@hotmail.com>
2021-05-18 20:30:19 -0300

    fix: no need of chainable

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,3 +1,4 @@
+/// <reference types="cypress" />
 // ***********************************************************
 // This example support/index.js is processed and
 // loaded automatically before your test files.
@@ -22,13 +23,3 @@ import './commands'
 Cypress.Commands.add('dataCy', (value) => {
   return cy.get(`[data-cy=${value}]`)
 })
-
-declare namespace Cypress {
-  interface Chainable {
-    /**
-     * Custom command to select DOM element by data-cy attribute.
-     * @example cy.dataCy('greeting')
-     */
-    dataCy(value: string): Chainable<Element>
-  }
-}
